c3cff35f4628a65bfbfa6b706757ae73bbc5fb6b
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2022-04-28 00:32:48 +0700

    I implemented a weather forecast

--- unit_14.js
+++ unit_14.js
@@ -8,13 +8,33 @@ let cityArr = [
   { id: 1489425, name: 'Tomsk' },
   { id: 1496153, name: 'Omsk' }
 ];
-
-// fetch("https://api.openweathermap.org/data/2.5/weather?id=1503901&appid=8940e5ef0e53a52a848430a215691da4")
-//   .then(function (resp) { return resp.json() })
-//   .then(function (data) {
-//     console.log(data);
-//     console.log(Math.round(data.main.temp - 273));
-//   })
-//   .catch(function () {
-
-//   });
+let objectMain = {
+  s1: document.querySelector('.s-1'),
+  out1: document.querySelector('.out-1')
+};
+function concatString(a) {
+  let b = 'https://api.openweathermap.org/data/2.5/weather?id=';
+  let c = '&appid=8940e5ef0e53a52a848430a215691da4';
+  return `${b}${a}${c}`;
+}
+function gValueOfS1() {
+  let valueOfS1 = objectMain.s1.value;
+  for (let i = 0; i < cityArr.length; i++) {
+    for (let key in cityArr[i]) {
+      if (cityArr[i].name == valueOfS1) {
+        return String(cityArr[i].id);
+      }
+    }
+  }
+}
+function getWeather() {
+  fetch(concatString(gValueOfS1()))
+    .then(function (resp) { return resp.json() })
+    .then(function (data) {
+      objectMain.out1.innerHTML = Math.round(data.main.temp - 273);
+      console.log(data);
+    })
+    .catch(function () {
+    });
+}
+objectMain.s1.addEventListener("change", getWeather);
