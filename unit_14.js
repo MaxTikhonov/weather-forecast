10c45f4dd096127aaa85f2f0a502bf4ffd09aaa6
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2022-04-27 23:17:56 +0700

    Add files via upload

---
+++ unit_14.js
@@ -0,0 +1,20 @@
+let cityArr = [
+  { id: 524894, name: 'Moscow' },
+  { id: 1496747, name: 'Novosibirsk' },
+  { id: 1503901, name: 'Kemerovo' },
+  { id: 1502026, name: 'Krasnoyarsk' },
+  { id: 2013348, name: 'Vladivostok' },
+  { id: 491422, name: 'Sochi' },
+  { id: 1489425, name: 'Tomsk' },
+  { id: 1496153, name: 'Omsk' }
+];
+
+// fetch("https://api.openweathermap.org/data/2.5/weather?id=1503901&appid=8940e5ef0e53a52a848430a215691da4")
+//   .then(function (resp) { return resp.json() })
+//   .then(function (data) {
+//     console.log(data);
+//     console.log(Math.round(data.main.temp - 273));
+//   })
+//   .catch(function () {
+
+//   });
